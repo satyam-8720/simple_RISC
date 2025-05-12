f0893e5e3ddb7edf56361f65ddf102db5fdfc53c
satyam <satyamchauhan181104@gmail.com>
2025-05-12 09:44:42 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-function assemble() {
-    const code = document.getElementById('input').value;
-    const output = assembleCode(code);
-    document.getElementById('output').textContent = output;
-}
